bdf5db7e9add9d5e7a914a65cd8878631a70747f
azeddineb <az-eddine.barbach@edu.uiz.ac.ma>
2023-03-16 20:59:22 +0000

    printf fct

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - a program that prints a line using printf
+ * Return: 0 (susccess)
+ */
+int main(void)
+{
+	/**
+	 * thsi instruction print a text
+	 * */
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}

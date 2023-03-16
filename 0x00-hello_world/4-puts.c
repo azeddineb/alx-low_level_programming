a2eb7213d41a5ed613a08de204677db48e1deac5
azeddineb <az-eddine.barbach@edu.uiz.ac.ma>
2023-03-16 20:33:26 +0000

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/**
+ * Entry point - a programme that print a line
+ * return: 0 (success)
+ */
+int main(){
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}

34ea18a23ada5ef952e96cbcc0c5c18d1ccb0fc4
azeddineb <az-eddine.barbach@edu.uiz.ac.ma>
2023-03-17 20:47:49 +0000

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - a program that print numbers from 0 to 10
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}

a8cd75a32cb939922be48bf1077013babbc64924
azeddineb <az-eddine.barbach@edu.uiz.ac.ma>
2023-03-17 21:20:20 +0000

    print numbers using putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,12 +7,12 @@ int main(void)
 {
 	int i = 0;
 
-	while (i < 10)
-	{
-		putchar(i + '0');
-                i++;
-	}
-	putchar('\n');
-	return (0);
+while (i < 10)
+{
+putchar(i + '0');
+i++;
+}
+putchar('\n');
+return (0);
 }
 

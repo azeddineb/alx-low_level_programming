7e3b6c0f55556a8ad71a1d151207aaba2ca1e294
azeddineb <az-eddine.barbach@edu.uiz.ac.ma>
2023-03-17 21:36:54 +0000

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - program prints all numbers of base 16
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	i = 'a';
+	while (i <= 'f')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+

b92539691d7972da551bd8bb393b243f7c385c58
azeddineb <az-eddine.barbach@edu.uiz.ac.ma>
2023-03-18 22:13:02 +0000

    print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,50 +1,31 @@
 #include <stdio.h>
+
 /**
- * main -program that prints all possible combinations of tow tow-digit numbers
- * Return: 0 (success)
+ * main - Prints all possible combinations of two two-digit numbers.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	int i, j, k, q, unique;
+    int i, j;
 
-	for (i = 0; i < 10; i++)
-	{
-		for (j = 0; j < 9; j++)
-		{
-			k = i;
-			while (k < 10)
-			{
-				q = (k == i) ? j + 1 : 0; /*start from j+1 only for the first value of k*/
-				while (q < 10)
-				{
-					unique = 1; /* assume combination is unique */
-					if (i == k && j == q) /* check if digits repeat */
-					{
-						unique = 0;
-					}
-					if (unique && i == q && j == k)
-					{
-						unique = 0;
-					}
-					if (unique)
-					{
-						putchar(i + '0');
-						putchar(j + '0');
-						putchar(' ');
-						putchar(k + '0');
-						putchar(q + '0');
-						if (i != 9 || j != 8 || k != 9 || q != 9)
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-					q++;
-				}
-				k++;
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+    for (i = 0; i <= 99; i++)
+    {
+        for (j = i + 1; j <= 99; j++)
+        {
+            putchar(i / 10 + '0');
+            putchar(i % 10 + '0');
+            putchar(' ');
+            putchar(j / 10 + '0');
+            putchar(j % 10 + '0');
+            if (i != 98 || j != 99)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+    putchar('\n');
+    return (0);
 }
+

8099d6313f6306768e210d76fbfd34680830a2e1
azeddineb <az-eddine.barbach@edu.uiz.ac.ma>
2023-03-18 16:32:52 +0000

    print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,34 +5,30 @@
  */
 int main(void)
 {
-	int i, j, k, q;
+    int i, j, k, l;
 
-	for (i = 0; i < 10; i++)
-	{
-		for (j = 0; j < 10; j++)
-		{
-			k = i;
-			while (k < 10)
-			{
-				q = (k == i) ? j+1 : 0; /*start from j+1 only for the first value of k*/
-				while (q < 10)
-				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(' ');
-					putchar(k + '0');
-					putchar(q + '0');
-					if (i != 9 || j != 9 || k != 9 || q != 9)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-					q++;
-				}
-				k++;
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+    for (i = 0; i <= 9; i++) {
+        for (j = i; j <= 9; j++) {
+            for (k = 0; k <= 9; k++) {
+                for (l = k; l <= 9; l++) {
+                    if (i*10 + j <= k*10 + l) {
+                        putchar(i + '0');
+                        putchar(j + '0');
+                        putchar(' ');
+                        putchar(k + '0');
+                        putchar(l + '0');
+                        if (i != 9 || j != 9 || k != 9 || l != 9) {
+                            putchar(',');
+                            putchar(' ');
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    putchar('\n');
+
+    return 0;
 }
+

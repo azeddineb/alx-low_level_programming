f68bcd54ca62cc9d0177bbf79b9af58352ff87d6
azeddineb <az-eddine.barbach@edu.uiz.ac.ma>
2023-03-22 23:53:36 +0000

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds tow integers and returns the result
+ * @a: parameter to add
+ * @b: parameter to add
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}

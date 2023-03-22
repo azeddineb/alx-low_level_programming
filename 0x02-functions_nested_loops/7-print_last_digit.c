2549c592397e69336f7915b432138d04fe1e9d72
azeddineb <az-eddine.barbach@edu.uiz.ac.ma>
2023-03-22 22:43:37 +0000

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,10 +6,13 @@
  */
 int print_last_digit(int n)
 {
-	int nbr;
+	int lastDigit;
 
-	nbr = n % 10;
-	_putchar(nbr);
-	return (nbr);
+	if (n < 0)
+		n = -n;
+	lastDigit = n % 10;
+	if (lastDigit < 0)
+		lastDigit = -lastDigit;
+	_putchar(a + '0');
+	return (lastDigit);
 }
-

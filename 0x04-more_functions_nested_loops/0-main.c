97da3af67622e9fef4150ea01be1ea8e3d55082a
azeddineb <az-eddine.barbach@edu.uiz.ac.ma>
2023-03-24 21:31:58 +0000

    update

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -10,8 +10,9 @@ int main(void)
 {
 	char c;
 
-	printf("%c: %d\n", c, _isupper(c));
 	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
 	printf("%c: %d\n", c, _isupper(c));
 	return (0);
 }

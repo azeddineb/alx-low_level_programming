582180cf042cb51b5f667b1e8feb0a2b944ae250
azeddineb <az-eddine.barbach@edu.uiz.ac.ma>
2023-03-24 20:50:43 +0000

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,16 +8,12 @@
 
 int _isupper(int c)
 {
-	int i;
-
-	for (i = 65; i <= 90; i++)
+	if (c >= 'A' && c <= 'Z')
 	{
-		if (c == i)
-		{
-			return (1);
-			break;
-		}
+		return (1);
+	} else
+	{
+		return (0);
 	}
-	return (0);
 }
 

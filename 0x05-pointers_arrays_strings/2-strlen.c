b5db14b40a697007d869c60deab09f73fa1836b1
azeddineb <az-eddine.barbach@edu.uiz.ac.ma>
2023-03-29 10:25:58 +0000

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - returns the lenght of a string
+ * @s: string
+ * @Return: lenght
+ */
+int _strlen(char *s)
+{
+	int lg = 0;
+
+	while (*s != '\0')
+	{
+		lg++;
+		s++;
+	}
+	return (lg);
+}

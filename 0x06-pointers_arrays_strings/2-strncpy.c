cf8065a2416007db0293c34e2e45e535d5150447
azeddineb <az-eddine.barbach@edu.uiz.ac.ma>
2023-04-01 13:43:55 +0000

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,12 +10,16 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (*src && i < n)
+	while (i < n && src[i] != '\0')
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		dest[i] = src[i];
+		i++;
 	}
-	*dest = '\0';
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 	return (dest);
+
 }

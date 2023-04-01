1f840a95d88b9444ee1362beebc8107a1207f9dd
azeddineb <az-eddine.barbach@edu.uiz.ac.ma>
2023-04-01 13:50:07 +0000

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: string
+ * @s2: string
+ * Return: s1[i]-s2[i]
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
+

b2b578b2bdc21edfcbc03c5dfc16e570678a357a
azeddineb <az-eddine.barbach@edu.uiz.ac.ma>
2023-04-17 21:28:31 +0000

    init dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,8 +1,10 @@
+#include <stdlib.h>
 #include "dog.h"
+
 /**
  * init_dog - initializes a variable of type struct dog
- * @d: pointer to struct dog
- * @name: name to inistialize
+ * @d: pointer to struct dog to initialize
+ * @name: name to initialize
  * @age: age to initialize
  * @owner: owner to initialize
  */

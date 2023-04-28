296649c37a9baf051f759658f83780b3ac40ec15
azeddineb <az-eddine.barbach@edu.uiz.ac.ma>
2023-04-28 13:00:00 +0000

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list
+ * @head: list_t list to be freed
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head;
+		free(tmp->str);
+		free(tmp);
+	}
+}
+

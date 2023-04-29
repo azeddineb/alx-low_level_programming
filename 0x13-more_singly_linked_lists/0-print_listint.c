bb7925ec32866a87665a8e20cf3cde5ea70a65f5
azeddineb <az-eddine.barbach@edu.uiz.ac.ma>
2023-04-29 00:18:57 +0000

    print listint

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+		printf("Error: the list is empty\n");
+	else
+	{
+		while (h)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			count++;
+		}
+	}
+
+	return (count);
+}
+

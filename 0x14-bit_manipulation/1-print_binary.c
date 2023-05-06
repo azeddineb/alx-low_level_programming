62ccc930f0c5a4935979b201dd3e74f01c6929cd
azeddineb <az-eddine.barbach@edu.uiz.ac.ma>
2023-05-06 15:35:12 +0000

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,9 +11,9 @@ void print_binary(unsigned long int n)
 
 	for (i = 63; i >= 0; i--)
 	{
-		current = n >> i;
+		curr = n >> i;
 
-		if (current & 1)
+		if (curr & 1)
 		{
 			_putchar('1');
 			counter++;

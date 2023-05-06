0b9ec94f8529bfac03cf82cf57f34f94175609ad
azeddineb <az-eddine.barbach@edu.uiz.ac.ma>
2023-05-06 15:42:09 +0000

    binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,22 +12,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
+	int i;
+	unsigned int dec_val = 0;
 
-	if (b == NULL)
-	{
+	if (!b)
 		return (0);
-	}
 
-	for (; *b != '\0'; b++)
+	for (i = 0; b[i]; i++)
 	{
-		if (*b != '0' && *b != '1')
-		{
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		}
-		result = (result << 1) + (*b - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
-	return (result);
+	return (dec_val);
 }
 
